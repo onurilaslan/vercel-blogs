183fc51e2af9ff6661021f3cc3d7e7dc6eab05e0
llenax <jaimitr14@gmail.com>
2021-12-29 20:31:55 +0300

    next.config.js rss feed import

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -5,9 +5,9 @@ const matter = require('gray-matter')
 
 async function generate() {
   const feed = new RSS({
-    title: 'Your Name',
-    site_url: 'https://yoursite.com',
-    feed_url: 'https://yoursite.com/feed.xml'
+    title: 'openduct blog',
+    site_url: 'https://blog.openduct.net',
+    feed_url: 'https://blog.openduct.net/feed.xml'
   })
 
   const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
