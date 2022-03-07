c3d68d0cf9acb80bb010e9e4ea4e41b71601eabb
llenax <jaimitr14@gmail.com>
2022-03-07 19:38:49 +0300

    '22-03-07.04'

--- pages/index.js
+++ pages/index.js
@@ -19,8 +19,24 @@ const App = ({ items }) => {
             // [guid, title, contentSnippet, link, creator, format(new Date(item.isoDate), 'PPP')]
             <></>
         ))} */}
-        <div className='w-full h-full bg-gradient-to-bl from-slate-900 to-slate-800'>
+        <div className='w-full h-full bg-gradient-to-bl from-slate-100 to-slate-300'>
+            <div className='w-full bg-slate-200 pt-2 p-3'>
+                <h4 className='text-slate-800 text-xl font-medium'>Onurİlaslan</h4>
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </div>
+            <div className='h-1/2 bg-slate-400'>
+                <div className='h-full w-1/2 bg-slate-500'>
+                    <h1 className='font-bold text-xl'>Onur blog</h1>
+                </div>
+                <div className='h-full w-1/2'>
 
+                </div>
+            </div>
         </div>
     </>
   )
